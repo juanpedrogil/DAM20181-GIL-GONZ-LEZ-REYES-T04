04489c931f90db2cd2bf48c049cfd4369359781a
ErnestoGonA <ergonzalezar@ittepic.edu.mx>
2018-04-05 16:44:31 -0500

    Push and get the data from firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,18 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCZqtAdmYFsHm5WPiSNcd-sGuRERZhyVs",
+  authDomain: "todo-list-6fb6e.firebaseapp.com",
+  databaseURL: "https://todo-list-6fb6e.firebaseio.com",
+  projectId: "todo-list-6fb6e",
+  storageBucket: "todo-list-6fb6e.appspot.com",
+  messagingSenderId: "517324328679"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -19,7 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -31,7 +45,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tasksList: AngularFireList<any>;
+  tasks: Observable<any[]>;
 
+  constructor(public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public afDatabase: AngularFireDatabase) {
+    this.tasksList = this.afDatabase.list('tasks');
+    this.tasks = this.tasksList.valueChanges();
   }
 
+  addTask() {
+    let prompt = this.alertCtrl.create({
+      title: 'Task Name',
+      message: "Enter a name for this new task you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            const newTaskRef = this.tasksList.push({});
+
+            newTaskRef.set({
+              id: newTaskRef.key,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
+
